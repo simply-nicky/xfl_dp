e0f68c9e91129bb851fa503c951994f7a60e728f
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-05-03 13:49:52 +0200

    geometry added

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import os, errno
-# from cfelpyutils.crystfel_utils import load_crystfel_geometry
-# from cfelpyutils.geometry_utils import apply_geometry_to_data
+from cfelpyutils.crystfel_utils import load_crystfel_geometry
+from cfelpyutils.geometry_utils import apply_geometry_to_data
 
 basepath = "/gpfs/exfel/u/scratch/MID/201802/p002200/cheetah/hdf5/r{0:04d}-{2:s}/XFEL-r{0:04d}-c{1:02d}.{3:s}"
 userpath = "XFEL-r{0:04d}-c{1:02d}.{2:s}"
@@ -9,7 +9,7 @@ datapath = "entry_1/instrument_1/detector_1/detector_corrected/data"
 trainpath = "/instrument/trainID"
 pulsepath = "/instrument/pulseID"
 
-# AGIPD_geom = load_crystfel_geometry("/home/vmariani/Workspaces-refactored/crystallography/karabo/agipd.geom")
+AGIPD_geom = load_crystfel_geometry("/home/vmariani/Workspaces-refactored/crystallography/karabo/agipd.geom")
 
 class worker_star(object):
     def __init__(self, worker):
@@ -21,8 +21,8 @@ class worker_star(object):
 def get_path_to_data(rnum, cnum, tag, ext, online):
     return basepath.format(rnum, cnum, tag, ext) if online else userpath.format(rnum, cnum, ext)
 
-# def apply_agipd_geom(frame):
-    # return apply_geometry_to_data(frame, AGIPD_geom)
+def apply_agipd_geom(frame):
+    return apply_geometry_to_data(frame, AGIPD_geom)
 
 def make_output_dir_and_path(path):
     abspath = os.path.join(os.path.dirname(__file__), path)
