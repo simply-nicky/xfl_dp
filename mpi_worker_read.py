49aa5f69cebc365021060f070c6cf99015f20dcd
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-06-05 13:52:12 +0200

    small fixes

--- mpi_worker_read.py
+++ mpi_worker_read.py
@@ -16,6 +16,7 @@ print('received')
 data_list, tids_list, pids_list = [], [], []
 ranges = chunkify(start_read, stop_read)
 comm.gather(len(ranges), root=0)
+print('gathered')
 for start, stop in ranges:
     _data_chunk, _tids_chunk, _pids_chunk = data_chunk(start, stop, cheetah_path, lim)
     data_list.append(_data_chunk); tids_list.append(_tids_chunk); pids_list.append(_pids_chunk)
