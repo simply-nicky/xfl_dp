fa77ace3521b1e150ae9f658e2ea1dc40cbfc42e
Nikolay Ivanov <nivanov@cfeld-macPro03.desy.de>
2019-06-05 17:29:00 +0200

    small fixes

--- src/utils.py
+++ src/utils.py
@@ -68,7 +68,7 @@ class MPIPool(object):
         for counter in range(pool_size):
             percent = (counter * 100) // pool_size
             print('\rProgress: [{0:<50}] {1:3d}%'.format('=' * (percent // 2), percent), end='\0')
-            self.comm.irecv(source=MPI.ANY_SOURCE, tag=tag)
+            self.comm.recv(source=MPI.ANY_SOURCE, tag=tag)
             print('ROOT received {}'.format(counter))
         else:
             print('\rProgress: [{0:<50}] {1:3d}%'.format('=' * 50, 100))
