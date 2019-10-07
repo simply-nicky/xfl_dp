712610e6c4398e4adc48d515cd8e3c40a6084397
simply-nicky <simply.i.nicky@gmail.com>
2019-10-07 10:23:29 +0200

    small change

--- exfel/data.py
+++ exfel/data.py
@@ -214,7 +214,7 @@ class ABCRawData(ABCData):
                     pid_data_list.append(data_chunk)
                     pid_data_list.append(gain_chunk)
                     pid_tids_list.append(tids_chunk)
-            print(len(pid_data_list))
+            print(len(pid_data_list), len(pid_gain_list))
             if pid_data_list:
                 data_list.append(np.concatenate(pid_data_list))
                 gain_list.append(np.concatenate(pid_gain_list))
